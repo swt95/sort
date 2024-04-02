f62690be609c3becd58a3deb90a6d36f4df34f86
swt <728682458@qq.com>
2024-04-02 16:54:13 +0800

    heap

--- heap_sort.c
+++ heap_sort.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void mySwap(int* a, int* b)
+//swap
+void swap(int * a, int * b)
 {
 	int temp = *a;
 	*a = *b;
 	*b = temp;
 }
+
+//维护堆
 void heapify(int arr[], int n, int i)
 {
 	int largest = i;
 	int lson = i * 2 + 1;
 	int rson = i * 2 + 2;
+
 	if(lson < n && arr[largest] < arr[lson])
 	{
 		largest = lson;
@@ -20,35 +24,40 @@ void heapify(int arr[], int n, int i)
 	{
 		largest = rson;
 	}
+
 	if(largest != i)
 	{
-		mySwap(&arr[largest], &arr[i]);
+		swap(&arr[largest], &arr[i]);
 		heapify(arr, n, largest);
 	}
+
+
 }
 
+//函数入口
 void heap_sort(int arr[], int n)
 {
-	int i;
-	for(i = n/2-1; i >= 0; i--)
+	//建堆
+	for(int i = n / 2 - 1; i >= 0; i--)
 	{
 		heapify(arr, n, i);
 	}
 
-	for(i = n-1; i >=0; i--)
+	//排序
+	for(int i = n-1; i >= 0; i--)
 	{
-		mySwap(&arr[0], &arr[i]);
+		swap(&arr[0], &arr[i]);
 		heapify(arr, i, 0);
-
 	}
 }
 
 int main()
 {
-	int arr[10] = {2,3,8,1,4,9,10,7,16,14};
+	int arr[10] = {2,8,6,5,14,16,7,15,1,13};
 	int n = 10;
+
 	heap_sort(arr, n);
-	for(int i = 0; i < n; i++)
+	for(int i =0; i < n; i++)
 	{
 		printf("%d ", arr[i]);
 	}
